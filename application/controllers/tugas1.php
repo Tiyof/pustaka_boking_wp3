107a3541950007aef4986061724fc014d08d4600
Alexanderrikky <alexanderrikky10@gmail.com>
2024-03-28 23:53:20 +0700

    tugas 1

---
+++ application/controllers/tugas1.php
@@ -0,0 +1,10 @@
+<?php
+class tugas1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan </h1>";
+        echo "Nama saya Alexander rikky saya tingal di jalan perdana no 51 lebih tepatnya asrama mahasiswa uncak kapuas
+        olah raga yang saya sukai adalah futsal";
+    }
+}
