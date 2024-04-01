e5984220af5ffccb49e668500bbb29ad549f8772
Alexanderrikky <alexanderrikky10@gmail.com>
2024-04-02 00:08:53 +0700

    NVC3

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan1</title>
+</head>
+<body>
+    Halo Kawan .. yuk kita belajar programming..!!<br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1. "+". $nilai2."=".$hasil; ?>
+</body>
+</html>
